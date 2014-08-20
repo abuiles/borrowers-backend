0119ecc22fb0cfd7980dbaaabf007b174134cff6
Adolfo Builes <builes.adolfo@gmail.com>
2014-08-20 15:37:05 -0500

    Start project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+BorrowersBackend::Application.config.secret_token = '8d3bcaf19b47008aa3f00c4af9dca35e2ab9a064d0ddf466b3628418ff3ea997334680887546ced8239ca089c4ab492fff97c8368f8a24754a280261eac2b6f9'
