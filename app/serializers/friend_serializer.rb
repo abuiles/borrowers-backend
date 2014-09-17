6b2ed293be767dbfea1a2875af0d3e51e2f03a82
Adolfo Builes <builes.adolfo@gmail.com>
2014-09-17 11:50:09 -0500

    Add naive articles count.

--- app/serializers/friend_serializer.rb
+++ app/serializers/friend_serializer.rb
@@ -1,3 +1,7 @@
 class FriendSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :twitter
+  attributes :id, :first_name, :last_name, :email, :twitter, :total_articles
+
+  def total_articles
+    object.articles.count
+  end
 end
