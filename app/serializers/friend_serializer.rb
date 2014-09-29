029fa160c1b7d18baf2188d69f80a1faba7b69dd
Adolfo Builes <builes.adolfo@gmail.com>
2014-09-29 11:06:05 -0500

    Sideload articles.

--- app/serializers/friend_serializer.rb
+++ app/serializers/friend_serializer.rb
@@ -1,6 +1,10 @@
 class FriendSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :email, :twitter, :total_articles
 
+  embed :ids
+
+  has_many :articles, include: true
+
   def total_articles
     object.articles.count
   end
