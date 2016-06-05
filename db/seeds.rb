4b141ead0be51cca9a7f72dce03b12a088932488
Oskar Niburski <oskarniburski@gmail.com>
2016-06-04 22:24:26 -0400

    trivial article

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
-
 friends = Friend.create([{ first_name: 'yo', last_name: 'Man', twitter: '@realDonaldTrump', email: 'ikno2@gmail.com' }, { first_name: 'yo', last_name: 'Man', twitter: '@realDonaldTrump', email: 'ikno3@gmail.com' }])
+
+
+friends.each do |friend|
+  20.times do |i|
+    borrow = i % 2 == 0 ? "borrowed" : "returned"
+    friend.articles.create({state: borrow, description: "#{i}"})
+  end
+end
 
